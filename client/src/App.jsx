a494b0233a75b5bd93d87be28cb209cd1c12ceee
Lucas Gomez Peña <gomezplucas@gmail.com>
2023-10-26 21:19:59 -0300

    Instalacion tailwind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { Dashboard } from './pages/Dashboard'
 
-function App() {
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch('http://localhost:4500/prueba')
-    .then(res => res.json())
-    .then(dat => {
-      setData(dat)
-      console.log(dat)
-    })
-  },[])
-
+function App () {
   return (
     <>
-      {
-        (typeof data.members === 'undefined')
-        ? (<p>Cargando</p>)
-        : (data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-          )
-        ))
-      }
+      <Dashboard />
     </>
   )
 }
