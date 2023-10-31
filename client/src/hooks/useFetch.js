270af011d4233938df57c0e7b474bfd662e2e491
Lucas Gomez Peña <gomezplucas@gmail.com>
2023-10-31 13:42:22 -0300

    Mejora de la presentacion de tabla productos

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,16 +1,6 @@
 import { useEffect, useState } from 'react'
 import { API_BASE_URL } from '../routes/apiUrl'
 
-// export const useFetch = ({ endpoint }) => {
-//   const [data, setData] = useState()
-
-//   useEffect(() => {
-//     fetch(API_BASE_URL + endpoint)
-//       .then(res => res.json())
-//       .then(res => { setData(res) })
-//   }, [endpoint])
-//   return { data }
-// }
 export const useFetch = ({ endpoint }) => {
   const [data, setData] = useState(null)
   const [isPending, setIsPending] = useState(false)
