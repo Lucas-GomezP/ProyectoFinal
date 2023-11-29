7cc49d12b4da9844a30874a92f05a8f7901f29d1
Lucas Gomez Peña <gomezplucas@gmail.com>
2023-11-29 09:29:31 -0300

    Arreglo cliente

--- server/api/routes/client.py
+++ server/api/routes/client.py
@@ -50,6 +50,18 @@ def create_client(user_id):
         # Se comprueban que los campos estén completos
         if not data or not all(campo in data for campo in CAMPOS_REQUERIDOS):           
             return jsonify({"message": "Datos incompletos"}), 400
+
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT COUNT(*) FROM clientes WHERE cuit_cuil = %s OR dni = %s OR email = %s', (data['cuitCuil'],  data['dni'], data['email']))
+        count = cur.fetchone()[0]             
+        cur = mysql.connection.cursor()      
+        # ---------------------------------------------------------------- esto se puede mover
+
+        # Si alguno de los valores ya existe, abortar la actualización
+        if count > 0:
+            print('aca')
+            return jsonify({"message": "Al menos uno de los valores ya existe en la tabla clientes"}),406
+
         
         # Crea una instancia de Cliente
         new_client = {
@@ -95,7 +107,7 @@ def create_client(user_id):
 @client_resource
 def update_client(user_id, client_id):
     try:
-        CAMPOS_REQUERIDOS = ['nombre', 'cuitCuil', 'apellido', 'dni', 'domicilio', 'telefono', 'email','estado']
+        CAMPOS_REQUERIDOS = ['nombre', 'cuitCuil', 'apellido', 'dni', 'domicilio', 'telefono', 'email']
 
         # Captura los datos en formato JSON
         data = request.get_json()   
@@ -107,14 +119,15 @@ def update_client(user_id, client_id):
         # ----------------------------------------------------------------
         # Verificar si los valores de cuitCuil, dni y email ya existen en la tabla clientes
         cur = mysql.connection.cursor()
-        cur.execute('SELECT COUNT(*) FROM clientes WHERE cuitCuil = %s OR dni = %s OR email = %s', (data['cuitCuil'],  data['dni'], data['email']))
+        cur.execute('SELECT COUNT(*) FROM clientes WHERE (cuit_cuil = %s OR dni = %s OR email = %s) AND id_cliente != %s', (data['cuitCuil'],  data['dni'], data['email'], client_id))
         count = cur.fetchone()[0]             
         cur = mysql.connection.cursor()      
         # ---------------------------------------------------------------- esto se puede mover
 
         # Si alguno de los valores ya existe, abortar la actualización
         if count > 0:
-            return jsonify({"message": "Al menos uno de los valores ya existe en la tabla clientes"}),500
+            print('aca')
+            return jsonify({"message": "Al menos uno de los valores ya existe en la tabla clientes"}),406
 
         # Actualiza el cliente en la base de datos
         consulta = 'UPDATE clientes SET nombre = %s, cuit_cuil = %s, apellido = %s, dni = %s, domicilio = %s, telefono = %s, email = %s, estado = %s WHERE id_cliente = %s AND id_usuario = %s'
